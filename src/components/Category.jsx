291f068f686059cea32ea95d129ef9ef4c149c0a
Prabakaran-M <mspk028@gmail.com>
2023-07-21 19:21:10 +0530

    colour changed

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { setCategory, setLoading } from "../redux/features/productSlice";
 
 const Category = () => {
+  const [selected, setSelected] = useState("All");
   const dispatch = useDispatch();
 
   const productCategory = ["All", "electronics", "clothing", "books"];
@@ -13,9 +14,14 @@ const Category = () => {
         {productCategory.map((catergory, index) => (
           <li
             key={index}
-            className="bg-slate-300 px-3 capitalize cursor-pointer py-1 rounded-md"
+            className={`${
+              selected === catergory
+                ? "bg-slate-800 text-white"
+                : "bg-slate-300"
+            } px-3 capitalize cursor-pointer py-1 rounded-md`}
             onClick={() => {
               dispatch(setLoading(true));
+              setSelected(catergory);
               setTimeout(() => {
                 dispatch(setCategory(catergory));
                 dispatch(setLoading(false));
